b3d2471c6441af1236cabfbd8b5e8c1101d4af35
João Fernando Corsini <jf.corsini@gmail.com>
2020-11-01 16:07:08 +0200

    Track data from temperature

--- tremor-ring.ino
+++ tremor-ring.ino
@@ -1,9 +1,11 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
+#include "Adafruit_Si7021.h"
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+Adafruit_Si7021 sensor = Adafruit_Si7021();
 
 void setup(void)
 {
@@ -11,11 +13,11 @@ void setup(void)
   Serial.println("Accelerometer Test"); Serial.println("");
 
   /* Initialise the sensor */
-  if (!accel.begin())
+  if (!accel.begin() || !sensor.begin())
   {
     /* There was a problem detecting the ADXL345 ... check your connections */
-    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
-    while (1);
+    Serial.println("Ooops, there is some problems with some sensor");
+    while (true);
   }
 
   /* Set the range to whatever is appropriate for your project */
@@ -35,6 +37,9 @@ void loop(void)
   /* Display the results (acceleration is measured in m/s^2) */
   Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
   Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  "); Serial.println("m/s^2 ");
-  delay(10);
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");
+  Serial.print("Humidity: ");Serial.print(sensor.readHumidity(), 2); Serial.print("  ");
+  Serial.print("Temperature: ");Serial.print(sensor.readTemperature(), 2); Serial.print("  ");
+  Serial.println("");
+  delay(100);
 }
